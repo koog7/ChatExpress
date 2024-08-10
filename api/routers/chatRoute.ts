64109c71480ce80cc668c13aa41626d0e2933756
koog7 <k.ognev709@mail.com>
2024-08-10 14:49:35 +0600

    Теперь при post на /messages есть проверка на пустую строку и так же присваивание id и date

--- api/routers/chatRoute.ts
+++ api/routers/chatRoute.ts
@@ -4,7 +4,13 @@ const chatRouter = express.Router();
 chatRouter.use(express.json());
 
 chatRouter.post('/messages', async (req, res) => {
-    res.send('123')
+    const {author , message} = req.body;
+
+    if(author.trim() === '' || message.trim() === ''){
+        return res.status(400).send({"error": "Author and message must be present in the request"})
+    }
+    const date = new Date().toISOString().replace(/:/g, '-');
+    res.send({id:crypto.randomUUID() ,author:author , message:message , date:date})
 });
 
 export default chatRouter;
