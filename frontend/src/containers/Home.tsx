0647bc0c973f7420c54f7797b65d7c714a717b32
koog7 <k.ognev709@mail.com>
2024-08-11 15:51:44 +0600

    Написана функция для Post запросов на серверную часть и использование ее на home странице

--- frontend/src/containers/Home.tsx
+++ frontend/src/containers/Home.tsx
@@ -1,16 +1,26 @@
-import React, {ChangeEvent, useState} from 'react';
+import {ChangeEvent, FormEvent, useState} from 'react';
 import {Button, TextField} from "@mui/material";
 import CardBlock from "../components/CardBlock.tsx";
+import {useDispatch} from "react-redux";
+import {createPost} from "./Slice/FetchSlice.ts";
 
 const Home = () => {
 
+
+    const dispatch = useDispatch();
     const [authorText , setAuthorText] = useState('')
     const [messageText , setMessageText] = useState('')
+
+
+    const submitPost = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        dispatch(createPost({author:authorText, message:messageText}))
+    }
 
     return (
         <div>
             <div>
-                <form style={{display:'flex', flexDirection:'column', gap:'10px', marginTop:'25px'}}>
+                <form style={{display:'flex', flexDirection:'column', gap:'10px', marginTop:'25px'}} onSubmit={submitPost}>
                     <TextField
                         id="outlined-controlled"
                         label="Author name"
@@ -73,12 +83,11 @@ const Home = () => {
                             },
                         }}
                     />
-                    <Button variant="contained">Send!</Button>
+                    <Button variant="contained" type={"submit"}>Send!</Button>
                 </form>
                 <div style={{marginTop:'20px'}}>
                     <CardBlock />
                 </div>
-
             </div>
         </div>
     );
--- frontend/src/containers/Slice/FetchSlice.ts
+++ frontend/src/containers/Slice/FetchSlice.ts
@@ -1,4 +1,5 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
+import axiosAPI from "../../axios/AxiosAPI.ts";
 
 interface MessageProps {
     author: string;
@@ -14,6 +15,24 @@ const initialState: MessageProps = {
     error: false,
 };
 
+
+export const createPost = createAsyncThunk<string, { author: string; message: string }>(
+    'chat/postMessage',
+    async ({ author, message } ) => {
+        try {
+            console.log('post try auth and msg' , author , message)
+            const response = await axiosAPI.post('/messages', {
+                author,
+                message,
+            });
+            console.log('response ', response)
+            return response.data;
+        } catch (error) {
+            return error.message;
+        }
+    }
+);
+
 const chatSlice = createSlice({
     name: 'chat',
     initialState,
@@ -23,7 +42,22 @@ const chatSlice = createSlice({
         },
     },
     extraReducers: (builder) => {
-
+        builder
+            .addCase(createPost.pending, (state:MessageProps) => {
+                state.loading = true;
+                state.error = false;
+            })
+            .addCase(createPost.fulfilled, (state:MessageProps, action: PayloadAction<string>) => {
+                state.loading = false;
+                state.error = false;
+                console.log(action.payload)
+                // state.author = action.payload.author;
+                // state.message = action.payload.author;
+            })
+            .addCase(createPost.rejected, (state:MessageProps) => {
+                state.loading = false;
+                state.error = true;
+            })
     },
 });
 
