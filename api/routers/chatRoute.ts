39a83963da84df5594cc299a0810f942f248ad1a
koog7 <k.ognev709@mail.com>
2024-08-12 13:12:18 +0600

    Добавлен get метод для получаения последних 30 сообщений , а так же получение сообщений после определнной даты

--- api/routers/chatRoute.ts
+++ api/routers/chatRoute.ts
@@ -10,11 +10,29 @@ chatRouter.post('/messages', async (req, res) => {
     if(author.trim() === '' || message.trim() === ''){
         return res.status(400).send({"error": "Author and message must be present in the request"})
     }
-    const date = new Date().toISOString().replace(/:/g, '-');
+    const date = new Date().toISOString();
 
     const msgData = {id:crypto.randomUUID() ,author:author , message:message , date:date}
     await fileDb.addItem(msgData);
     res.send(msgData)
+});
+
+
+chatRouter.get('/messages', async (req, res) => {
+    try{
+        const queryDate = req.query.datetime as string;
+        const messages = await fileDb.getItems();
+        if(queryDate){
+            const date = new Date(queryDate);
+            const filteredMessages = messages.filter(message => new Date(message.date) > date);
+            res.send(filteredMessages)
+        }else{
+            res.send(messages.slice(-30))
+        }
+    }catch (e) {
+        res.status(400).send({"error": "Some problems with fetching data"})
+    }
+
 });
 
 export default chatRouter;
